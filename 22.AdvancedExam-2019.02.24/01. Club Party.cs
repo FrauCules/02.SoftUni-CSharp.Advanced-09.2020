ab2c8fa29103744f97618292d2da52d3befb32c6
FrauCules <71455639+FrauCules@users.noreply.github.com>
2020-10-27 15:28:44 +0200

    Add files via upload

---
+++ 22.AdvancedExam-2019.02.24/01. Club Party.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Club_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // n – an integer specifying the halls' maximum capacity
+            // Then you will be given input line which will contain English alphabet letters and numbers, separated by a single space
+            // The input for the line should be read from the last inserted to the first one.
+            // he letters represent the halls and the numbers – the people in a single reservation.
+            // Companies of people should go in the halls. 
+            // The first entered hall is the first which people are entering. 
+            // Every reservation takes specific capacity, equal to its number.
+            // Input:
+            //  60
+            // 1 20 b 20 20 a
+            // Output
+            // a-> 20, 20, 20
+
+            int capacity = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+            Stack<string> stack = new Stack<string>();    // пази входа отзад напред
+            Queue<string> halls = new Queue<string>();    // пази реда и дали има отворена зала
+            List<int> reservations = new List<int>();
+            int count = 0;                                // колко човека има в текущата зала  
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            while (stack.Count > 0) // 50    - 15 a 40 30 20 c 15 10 b
+
+            {
+                string currentInput = stack.Peek();
+                if (char.IsDigit(currentInput[0])) // резервация ли е
+                {
+                    int reservation = int.Parse(currentInput);
+                    if (halls.Count > 0)            // има ли отворена зала
+                    {
+                        if(count + reservation <= capacity) // отворената зала побира ли текущата резервация
+                        {
+                            reservations.Add(reservation); // ако ДА - добавяме я в списъка с влезлите резервации
+                            count += reservation;
+                        }
+                        else   // ако залата прелива - разпечатваме текущата залата и влезлите разервации
+                        {
+                            Console.WriteLine($"{halls.Dequeue()} -> {string.Join(", ",reservations)}");
+                            reservations.Clear();
+                            count = 0;
+
+                            if (halls.Count > 0)
+                            {
+                                reservations.Add(reservation); // ако ДА - добавяме я в списъка с влезлите резервации
+                                count += reservation;
+                            }
+                        }
+                    }  // ако няма отворена зала, пропускаме резервацията
+                }
+                else
+                {
+                    halls.Enqueue(currentInput);
+                }
+                stack.Pop();
+            }
+        }
+    }
+}
---
+++ 22.AdvancedExam-2019.02.24/02. Tron Racers.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace _02._Tron_Racers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            ReadMatrix(matrix);
+            int posFrow = PositionX(matrix, 'f');
+            int posFcol = PositionY(matrix, 'f');
+            int posSrow = PositionX(matrix, 's');
+            int posScol = PositionY(matrix, 's');
+
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                string[] command = Console.ReadLine().Split();
+                string directionF = command[0];
+                string directionS = command[1];
+
+                switch (directionF)
+                {
+                    case "down":
+                        if (posFrow + 1 < n)
+                        {
+                            posFrow++;
+                        }
+                        else
+                        {
+                            posFrow=0;
+                        }
+                        break;
+                    case "up":
+                        if (posFrow - 1 < 0)
+                        {
+                            posFrow = n-1;
+                        }
+                        else
+                        {
+                            posFrow--;
+                        }
+                        break;
+                    case "left":
+                        if (posFcol - 1 < 0)
+                        {
+                            posFcol = n - 1;
+                        }
+                        else
+                        {
+                            posFcol--;
+                        }
+                        break;
+                    case "right":
+                        if (posFcol + 1 < n)
+                        {
+                            posFcol++;
+                        }
+                        else
+                        {
+                            posFcol = 0;
+                        }
+                        break;
+                }
+
+                if(matrix[posFrow, posFcol] == 's')
+                {
+                    matrix[posFrow, posFcol] = 'x';
+                    break;
+                }
+                else
+                {
+                    matrix[posFrow, posFcol] = 'f';
+                }
+
+                switch (directionS)
+                {
+                    case "down":
+                        if (posSrow + 1 < n)
+                        {
+                            posSrow++;
+                        }
+                        else
+                        {
+                            posSrow = 0;
+                        }
+                        break;
+                    case "up":
+                        if (posSrow - 1 < 0)
+                        {
+                            posSrow = n - 1;
+                        }
+                        else
+                        {
+                            posSrow--;
+                        }
+                        break;
+                    case "left":
+                        if (posScol - 1 < 0)
+                        {
+                            posScol = n - 1;
+                        }
+                        else
+                        {
+                            posScol--;
+                        }
+                        break;
+                    case "right":
+                        if (posScol + 1 < n)
+                        {
+                            posScol++;
+                        }
+                        else
+                        {
+                            posScol = 0;
+                        }
+                        break;
+                }
+
+                if (matrix[posSrow, posScol] == 'f')
+                {
+                    matrix[posSrow, posScol] = 'x';
+                    break;
+                }
+                else
+                {
+                    matrix[posSrow, posScol] = 's';
+                }
+
+            }
+
+            PrintMatrix(matrix); ;
+        }
+
+        private static int PositionX(char[,] matrix, char player)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == player)
+                    {
+                        return row;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static int PositionY(char[,] matrix, char player)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == player)
+                    {
+                        return col;
+                    } 
+                }
+            }
+
+            return -1;
+        }
+        private static void ReadMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] currentRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
