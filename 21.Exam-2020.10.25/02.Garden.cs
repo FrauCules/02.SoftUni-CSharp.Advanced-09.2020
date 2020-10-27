7759da075a6278570db2fdb8207e23a5c47aa4c9
FrauCules <71455639+FrauCules@users.noreply.github.com>
2020-10-27 15:33:08 +0200

    Add files via upload

---
+++ 21.Exam-2020.10.25/02.Garden.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimention = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[dimention[0], dimention[1]];
+            FillMatrix(matrix);
+            Queue<int> flowers = new Queue<int>();
+            
+            string line;
+            while ((line = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] newPpositionF = line.Split().Select(int.Parse).ToArray();
+                int rowF = newPpositionF[0];
+                int colF = newPpositionF[1];
+                if (rowF < 0 || rowF >= dimention[0] || colF < 0 || colF >= dimention[1])
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+                matrix[rowF, colF] = 1;
+                flowers.Enqueue(rowF);
+                flowers.Enqueue(colF);
+            }
+
+            BloomBloomPlow(matrix, flowers);
+
+            PrintMatrix(matrix);
+        }
+
+        private static void BloomBloomPlow(int[,] matrix, Queue<int> flowers)
+        {
+            while (flowers.Count > 0)
+            {
+                int row = flowers.Dequeue();
+                int col = flowers.Dequeue();
+
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    if (col != i)
+                        matrix[row, i]++;
+                }
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    if (row != i)
+                        matrix[i, col]++;
+                }
+            }
+        }
+
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 21.Exam-2020.10.25/03. Classroom_Skeleton/Classroom.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    class Classroom
+    {
+
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            this.students = new List<Student>();
+        }
+        public int Capacity { get; set; }
+
+        public int Count =>
+            this.students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if(students.Count< this.Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return $"No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            bool  exist = students.Any(x => x.FirstName == firstName && x.LastName == lastName);
+            if (exist)
+            {
+                students = students.Where(x => x.FirstName != firstName && x.LastName != lastName).ToList();
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            bool exist = students.Any(x => x.Subject== subject);
+            if (exist)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (var item in students.Where(x => x.Subject == subject))
+                {
+                    sb.AppendLine($"{item.FirstName} {item.LastName}");
+                }
+
+                return sb.ToString().Trim();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+        public int GetStudentsCount()
+        {
+            return students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            bool exist = students.Any(x => x.FirstName == firstName && x.LastName == lastName);
+            if (exist)
+            {
+                var getStudent = students.Where(x => x.FirstName == firstName && x.LastName == lastName).First();
+
+                return getStudent;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
